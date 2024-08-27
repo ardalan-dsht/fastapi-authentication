1c2169aca6c0332e694e70bd5ad4d8c0c29c4d1b
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-08-27 20:27:09 +0330

    reformat files with black

--- fastapi_authentication/__init__.py
+++ fastapi_authentication/__init__.py
@@ -1,7 +1,7 @@
-from .schemas import Token, User
 from .auth import (
-    get_current_active_user,
     authenticate_user,
     create_access_token,
     db_users_collection,
+    get_current_active_user,
 )
+from .schemas import Token, User
